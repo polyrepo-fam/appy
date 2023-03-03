85883ec31f895e0435a9cebe7df61679258f6b07
Jonathan Brink <jonathandavidbrink@gmail.com>
2023-03-03 15:32:48 -0500

    chore: import

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Button, Heading } from "@jonbri/libby-ui";
+function App() {
+  return (
+    <div>
+      <h1>appy</h1>
+      <div>
+        <h2>@jonbri/libby-ui</h2>
+        <Button>Hello, Button!</Button>
+        <Heading level={3}>Hello, Heading</Heading>
+      </div>
+    </div>
+  );
+}
+
+export default App;
