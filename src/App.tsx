f2f2a999194a47954f981681489d8028882e0ae2
Jonathan Brink <jonathandavidbrink@gmail.com>
2023-03-03 16:54:58 -0500

    chore: use @polyrepo-fam

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import { Button, Heading } from "@jonbri/libby-ui";
+import { Button, Heading } from "@polyrepo-fam/libby-ui";
 function App() {
   return (
     <div>
       <h1>appy</h1>
       <div>
-        <h2>@jonbri/libby-ui</h2>
+        <h2>@polyrepo-fam/libby-ui</h2>
         <Button>Hello, Button!</Button>
         <Heading level={3}>Hello, Heading</Heading>
       </div>
