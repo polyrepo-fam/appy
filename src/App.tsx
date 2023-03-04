d0de09ac6f2edf094f0d032a751fc21710b79e7d
Jonathan Brink <jonathandavidbrink@gmail.com>
2023-03-03 21:48:26 -0500

    feat: index

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Button, Heading } from "@polyrepo-fam/libby-ui";
 function App() {
   return (
     <div>
+      <a href="https://github.com/polyrepo-fam">polyrepo-fam</a>
       <h1>appy</h1>
       <div>
         <h2>@polyrepo-fam/libby-ui</h2>
